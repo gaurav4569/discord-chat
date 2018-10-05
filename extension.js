da33292aa8b24874804e6bd0f0fe152552277374
Nigel Scott <nige@zaonce.com>
2018-10-05 19:07:46 +0100

    Fix auto hide cancel

--- extension.js
+++ extension.js
@@ -731,10 +731,7 @@ function activate( context )
         {
             if( e && e.document )
             {
-                streams.findOutputChannel( function( channel )
-                {
-                    streams.cancelAutoHide( channel );
-                } );
+                streams.cancelAutoHide( streams.getChannelId( e.document.fileName ) );
             }
         } ) );
 

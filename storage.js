23de39836eab38b6eab7649fe7c88be90d638e63
Nigel Scott <nscott@asl-control.co.uk>
2019-02-15 14:01:15 +0000

    Version 0.3.0

--- storage.js
+++ storage.js
@@ -121,8 +121,11 @@ function initialize( workspaceState )
 
 function setActive( isActive )
 {
+    if( isActive === false )
+    {
+        backup();
+    }
     active = isActive;
-
 }
 
 function backup()
